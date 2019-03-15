11b7f69745f18bf1984612902b06bdebb6506af2
Rashmi Mohod <rashmi.mohod@ignitesol.com>
2019-03-15 16:15:46 +0530

    Implementation: Update the varible names and condition check for image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,14 +12,14 @@ export class AppComponent {
   height: number;
 
   constructor() {
-    this.paths = [{imageUrl: 'https://hallysonh.github.io/ngx-imageviewer/pdf-test.pdf',
-    mime: 'application/pdf'},
-    {imageUrl: 'https://i.ytimg.com/vi/nlYlNF30bVg/hqdefault.jpg',
-    mime: 'image/jgeg'},
-    {imageUrl: 'https://www.askideas.com/media/10/Funny-Goat-Closeup-Pouting-Face.jpg',
-    mime: 'image/jgeg'},
-    {imageUrl: 'https://en.wikipedia.org/wiki/Earth',
-    mime: 'text/html'}
+    this.paths = [{url: 'https://hallysonh.github.io/ngx-imageviewer/pdf-test.pdf',
+    mimetype: 'application/pdf'},
+    {url: 'https://i.ytimg.com/vi/nlYlNF30bVg/hqdefault.jpg',
+    mimetype: 'image/png'},
+    {url: 'https://www.askideas.com/media/10/Funny-Goat-Closeup-Pouting-Face.jpg',
+    mimetype: 'image/jpeg'},
+    {url: 'https://en.wikipedia.org/wiki/Earth',
+    mimetype: 'text/html'}
   ];
 
   }
